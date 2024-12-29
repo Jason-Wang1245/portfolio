411f726435d36105bc61b1ff75dcf589aae22c7b
Jason-Wang1245 <jasonwang1245@gmail.com>
2024-12-29 18:29:15 -0500

    Updated spline url

--- app/Intro.tsx
+++ app/Intro.tsx
@@ -1,10 +1,13 @@
+"use client";
+
 import Spline from "@splinetool/react-spline";
 
 export default function Intro() {
+
   return (
     <div className="intro-container">
       <div className="spline-background">
-        <Spline style={{ height: "110vh" }} scene="https://prod.spline.design/LNaa21PLqFzpwWuo/scene.splinecode" />
+        <Spline style={{ height: "110vh" }} scene="https://prod.spline.design/mtne7ggQp72SlTvd/scene.splinecode" />
       </div>
       <div className="intro-content">
         <div className="grid place-content-center h-screen">Hello</div>
