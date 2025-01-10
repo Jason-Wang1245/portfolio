326231b7bdbf5353c2a05a38155330e51a8e5af7
Jason-Wang1245 <jasonwang1245@gmail.com>
2025-01-10 16:47:27 -0500

    Added contact icons in Details section

--- app/Details.tsx
+++ app/Details.tsx
@@ -25,52 +25,67 @@ export default function Details() {
       style={{ background: `radial-gradient(600px at ${mousePosition.x}px ${mousePosition.y}px, #051e2e, #030712)` }}
       onMouseMove={handleMouseMove}
     >
-      <div className="m-10 mt-20 flex flex-col basis-3/12">
-        <div>
-          <button
-            className={`topic-button hover:text-cyan-500 px-4 py-2 transition-all duration-300 rounded-xl bg-opacity-10 ${
-              activeTopic === "About Me" ? "text-cyan-500 bg-white shadow-2xl" : "text-white"
-            }`}
-            onClick={() => {
-              handleTopicChange("About Me");
-            }}
-          >
-            About Me
-          </button>
+      <div className="m-10 mt-20 flex flex-col basis-3/12 justify-between pb-12">
+        <div className="flex flex-col">
+          <div>
+            <button
+              className={`topic-button hover:text-cyan-500 px-4 py-2 transition-all duration-300 rounded-xl bg-opacity-10 ${
+                activeTopic === "About Me" ? "text-cyan-500 bg-white shadow-2xl" : "text-white"
+              }`}
+              onClick={() => {
+                handleTopicChange("About Me");
+              }}
+            >
+              About Me
+            </button>
+          </div>
+          <div>
+            <button
+              className={`topic-button hover:text-cyan-500 px-4 py-2 transition-all duration-300 rounded-xl bg-opacity-10 ${
+                activeTopic === "Experience" ? "text-cyan-500 bg-white shadow-2xl" : "text-white"
+              }`}
+              onClick={() => {
+                handleTopicChange("Experience");
+              }}
+            >
+              Experience
+            </button>
+          </div>
+          <div>
+            <button
+              className={`topic-button hover:text-cyan-500 px-4 py-2 transition-all duration-300 rounded-xl bg-opacity-10 ${
+                activeTopic === "Projects" ? "text-cyan-500 bg-white shadow-2xl" : "text-white"
+              }`}
+              onClick={() => {
+                handleTopicChange("Projects");
+              }}
+            >
+              Projects
+            </button>
+          </div>
         </div>
-        <div>
-          <button
-            className={`topic-button hover:text-cyan-500 px-4 py-2 transition-all duration-300 rounded-xl bg-opacity-10 ${
-              activeTopic === "Experience" ? "text-cyan-500 bg-white shadow-2xl" : "text-white"
-            }`}
-            onClick={() => {
-              handleTopicChange("Experience");
-            }}
-          >
-            Experience
-          </button>
-        </div>
-        <div>
-          <button
-            className={`topic-button hover:text-cyan-500 px-4 py-2 transition-all duration-300 rounded-xl bg-opacity-10 ${
-              activeTopic === "Projects" ? "text-cyan-500 bg-white shadow-2xl" : "text-white"
-            }`}
-            onClick={() => {
-              handleTopicChange("Projects");
-            }}
-          >
-            Projects
-          </button>
+        <div className="flex gap-4 pl-2">
+          <a href="https://www.linkedin.com/in/jasonwang1123/" target="_blank">
+            <i className="bi bi-linkedin text-3xl" />
+          </a>
+          <a href="https://github.com/Jason-Wang1245" target="_blank">
+            <i className="bi bi-github text-3xl" />
+          </a>
+          <a href="mailto:jasonwang1245@gamil.com" target="_blank">
+            <i className="bi bi-envelope-at-fill text-3xl" />
+          </a>
         </div>
       </div>
-      <div className="m-10 mt-20 basis-9/12 relative overflow-scroll overflow-y-scroll no-scrollbar" style={{scrollbarGutter: "stable"}}>
+      <div className="m-10 mt-20 basis-9/12 relative overflow-scroll overflow-y-scroll no-scrollbar" style={{ scrollbarGutter: "stable" }}>
         <div className={`absolute duration-300 transition-all ${activeTopic === "About Me" ? "opacity-100" : "opacity-0 hidden"}`}>
           <AboutMe />
         </div>
         <div className={`w-full absolute duration-300 transition-all ${activeTopic === "Experience" ? "opacity-100" : "opacity-0 hidden"}`}>
           <Experience />
         </div>
-        <div className={`absolute duration-300 transition-all ${activeTopic === "Projects" ? "opacity-100" : "opacity-0 hidden"}`}>coming soon...</div>
+        <div className={`absolute duration-300 transition-all ${activeTopic === "Projects" ? "opacity-100" : "opacity-0 hidden"}`}>
+          coming soon...
+        </div>
       </div>
     </div>
   );
